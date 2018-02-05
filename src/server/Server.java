e85c3de1c3315d167dedffda276010495f9bf198
MikovIvan <mikov_ivan@list.ru>
2018-02-05 21:18:08 +0300

    add synchronization

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,38 +11,47 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
-public class Server {
-    private final int PORT = 8189;
+import static server.database.ConnectSQLiteServer.connectDB;
+import static server.database.ConnectSQLiteServer.disconnectDB;
+
+
+public class Server implements Const{
+
     private List<ClientHandler> clients;
-    private AuthService authService;
-    public AuthService getAuthService(){
-        return authService;
-    }
-    //private List<ClientHandler> synClients = Collections.synchronizedList(clients);
 
     public Server() {
         ServerSocket serverSocket = null;
         Socket socket = null;
-        ConnectSQLiteServer connectSQLiteServer = null;
-        clients = new ArrayList<>();
+        clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
         try {
             serverSocket = new ServerSocket(PORT);
-            authService = new BaseAuthService();
-            authService.start();
-            connectSQLiteServer = new ConnectSQLiteServer();
-            //connectDB();
+            connectDB();
             System.out.println("Server start, wait clients");
 
             while (true) {
                 socket = serverSocket.accept();
-                clients.add(new ClientHandler(socket, this));
+                synchronized (clients){
+                    clients.add(new ClientHandler(socket, this));
+                }
                 System.out.println("Client connected");
             }
-        } catch (IOException e) {
-//            socket.close();
-//            connectSQLiteServer.disconnectDB();
+
+        } catch (SQLException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                serverSocket.close();
+                disconnectDB();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -50,25 +59,4 @@ public class Server {
         clients.remove(c);
     }
 
-    public boolean isNickBusy(String nick){
-        System.out.println(nick);
-        for(ClientHandler c: clients){
-            if(c.getName().equals(nick)) return true;
-        }
-        return false;
-    }
-    public void broadcast(String msg){
-        for(ClientHandler c: clients){
-            c.sendMessage(msg);
-        }
-    }
-    public void broadcastUsersList(){
-        StringBuffer sb = new StringBuffer("/userslist");
-        for(ClientHandler c: clients){
-            sb.append(" " + c.getName());
-        }
-        for(ClientHandler c: clients){
-            c.sendMessage(sb.toString());
-        }
-    }
 }
