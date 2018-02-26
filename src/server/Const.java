318d5796296c10b3d8dce3f447aa68f3b5b4d2a5
MikovIvan <mikov_ivan@list.ru>
2018-02-26 23:15:05 +0300

    delete SQL query

--- src/server/Const.java
+++ src/server/Const.java
@@ -2,6 +2,6 @@ package server;
 
 public interface Const {
     int PORT = 8888;
-    String SERVER_IP = "192.168.100.4";
-    String SELECT_USER = "SELECT * from users where email=? and password=?";
+    String SERVER_IP = "192.168.100.6";
+
 }
